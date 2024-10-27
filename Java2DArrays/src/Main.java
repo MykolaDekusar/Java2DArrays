3beac8ddcaafecb44f3538ccf04e92582af46e85
MykolaDekusar <ndekusar97@gmail.com>
2024-10-27 15:47:57 +0100

    Creation of 2d string using String[][] macchine= new String[2][2]; with assegnation of values for each row and column

--- Java2DArrays/src/Main.java
+++ Java2DArrays/src/Main.java
@@ -2,6 +2,18 @@
 public class Main {
 
 	public static void main(String[] args) {
+		//un array 2d è un array contenete altri array
+		//inizializziamo un array stringa 2d
+		//si utilizzano[][] per indicare un array 2d
+		//Il primo [] contiene le righe
+		//Il secondo [] contiene le colonne
+		String[][] macchine= new String[2][2];
+		//inseriamo i dati
+		macchine[0][0]="Subaru";  //riempio la prima riga e le 2 colonne
+		macchine[0][1]="Mazda";	  //riempio la prima riga e le 2 colonne
+		
+		macchine[1][0]="Citroen";  //riempio la seconda riga e le 2 colonne
+		macchine[1][1]="Hyundai";  //riempio la seconda riga e le 2 colonne
 		
 	}
 
